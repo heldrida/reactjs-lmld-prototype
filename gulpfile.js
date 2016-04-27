919a16525af80d01308703f593ff2b67d063e7bc
Helder <helder@lamoulade.com>
2016-04-27 11:57:21 +0100

    added hashfiles from webpack dev server to the gitignore file; modified the gulpfile to watch and inject on index.html modified;

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,14 @@ var gulp = require('gulp'),
 	webpack = require("webpack"),
 	WebpackDevServer = require("webpack-dev-server"),
 	webpackConfig = require("./webpack.config.js"),
-	gutil = require('gulp-util');
-
+	gutil = require('gulp-util'),
+	inject = require('gulp-inject');
 
 gulp.task('sass', function () {
     return gulp.src('./src/sass/**/*.scss')
         .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest('./build/css'));
-});
-
-gulp.task('watch', function () {
-	gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
 
 gulp.task("webpack:build", function(callback) {
@@ -44,23 +40,41 @@ gulp.task("webpack:build", function(callback) {
 });
 
 gulp.task("webpack:server", function(callback) {
+
 	// modify some webpack config options
 	var myConfig = Object.create(webpackConfig);
 	myConfig.devtool = "eval";
 	myConfig.debug = true;
 
 	// Start a webpack-dev-server
-	new WebpackDevServer(webpack(myConfig), {
+	var server = new WebpackDevServer(webpack(myConfig), {
+		contentBase: './build',
 		hot: true,
 		progress: true,
 		open: true,
 		stats: {
 			colors: true
 		}
-	}).listen(8888, "localhost", function(err) {
+	});
+
+	server.listen(8888, "localhost", function(err) {
 		if(err) throw new gutil.PluginError("webpack-dev-server", err);
 		gutil.log("[webpack-dev-server]", "http://localhost:8888");
 	});
+
+});
+
+gulp.task("inject", function () {
+
+	return gulp.src('./build/index.html')
+			.pipe(inject(gulp.src('./build/css/**/*.css', {read: false}), {relative: true}))
+			.pipe(gulp.dest('./build'));
+
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
+	gulp.watch('./build/index.html', ['inject']);
 });
 
 gulp.task('default', ['webpack:server', 'watch']);
