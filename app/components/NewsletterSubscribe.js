8f2286e5025ea3bf010a52de951c847712238e7f
Helder <helder@lamoulade.com>
2016-04-27 17:59:07 +0100

    Created component placeholder Newsletter subscribe; Created initial styles;

---
+++ app/components/NewsletterSubscribe.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class NewsletterSubscribe extends React.Component {
+
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+
+		return (
+			<div className="newsletter-subscribe">
+				<form action="getform.php" method="get">
+					<fieldset>
+						<input type="email" placeholder="Enter your email" />
+					</fieldset>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
+
+	}
+
+}
+
+export default NewsletterSubscribe;
--- app/containers/Footer.js
+++ app/containers/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewsletterSubscribe from '../components/NewsletterSubscribe.js';
 
 class Footer extends React.Component {
 
@@ -12,6 +13,7 @@ class Footer extends React.Component {
 			<div>
 				<footer>
 					<h6>Get updates from our latest projects</h6>
+					<NewsletterSubscribe />
 				</footer>
 			</div>
 		);
