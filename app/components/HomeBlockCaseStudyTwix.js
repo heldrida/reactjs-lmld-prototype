2366fb58d18ebc4748603f899246e309f58a8192
Helder <heldrida@gmail.com>
2016-05-03 23:53:23 +0100

    added text

--- app/components/HomeBlockCaseStudyTwix.js
+++ app/components/HomeBlockCaseStudyTwix.js
@@ -14,7 +14,7 @@ class HomeBlockCaseStudyTwix extends React.Component {
 
 		return (
 			<div class="case-study-twix">
-				<h1>CASE STUDY TWIX</h1>
+				<h1>Homeblock Component: CASE STUDY TWIX</h1>
 				<p>Non igitur potestis voluptate omnia dirigentes aut tueri aut retinere virtutem. Non pugnem cum homine, cur tantum habeat in natura boni; Qua tu etiam inprudens utebare non numquam. Universa enim illorum ratione cum tota vestra confligendum puto. Duo Reges: constructio interrete.</p>
 			</div>
 		);
