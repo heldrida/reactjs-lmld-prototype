27b5d9ce7bff0d24859b568743a425bc0964b2f6
Helder <heldrida@gmail.com>
2016-05-03 23:51:44 +0100

    added some dummy text to the home blocks

--- app/components/HomeBlockCaseStudyVans.js
+++ app/components/HomeBlockCaseStudyVans.js
@@ -15,6 +15,7 @@ class HomeBlockCaseStudyVans extends React.Component {
 		return (
 			<div class="case-study-vans">
 				<h1>CASE STUDY VANS</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non igitur potestis voluptate omnia dirigentes aut tueri aut retinere virtutem. Non pugnem cum homine, cur tantum habeat in natura boni; Qua tu etiam inprudens utebare non numquam. Universa enim illorum ratione cum tota vestra confligendum puto. Duo Reges: constructio interrete.</p>
 			</div>
 		);
 
