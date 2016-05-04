5b7ce92822f40f1bc45480b2d4326a714f0b6c6f
Helder <helder@lamoulade.com>
2016-05-04 15:32:20 +0100

    fix mixed tabs and spaces

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,11 @@ module.exports = {
 			{ test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
             { test: /\.scss$/, loader: 'style!css?sourceMap!sass?sourceMap' },
 			{
-			    test: /\.(jpe?g|png|gif|svg)$/i,
-			    loaders: [
-			        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-			        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-			    ]
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file?hash=sha512&digest=hex&name=[hash].[ext]',
+					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+				]
 			}
 		]
 	},
