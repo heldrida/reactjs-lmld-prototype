a1d347ed06ff4fc8a21259191bbc86674f21bc53
Helder <helder@lamoulade.com>
2016-05-04 15:30:07 +0100

    Installed and configured the image-loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,14 @@ module.exports = {
 	module: {
 		loaders: [
 			{ test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
-            { test: /\.scss$/, loader: 'style!css?sourceMap!sass?sourceMap' }
+            { test: /\.scss$/, loader: 'style!css?sourceMap!sass?sourceMap' },
+			{
+			    test: /\.(jpe?g|png|gif|svg)$/i,
+			    loaders: [
+			        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+			        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+			    ]
+			}
 		]
 	},
 	plugins: [
