cb7d3d0fd06eff1517e3e878a6e37e3bf3159b7d
Helder <heldrida@gmail.com>
2016-05-09 16:26:35 +0100

    refactored the backBlock, from stateless to state

---
+++ app/components/BackBlock.js
@@ -0,0 +1,19 @@
+'user strict';
+
+import React from 'react';
+
+class BackBlock extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className={'center-block'}></div>
+		);
+	}
+
+}
+
+export default BackBlock;
