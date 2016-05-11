db760385a539caf3f6f905b15af91c24ee96f56e
Charles de Dreuille <cdedreuille@Charles-MBP.home>
2016-05-11 19:01:49 +0100

    Responsive on homepage

--- app/components/home/YellowHello.js
+++ app/components/home/YellowHello.js
@@ -10,7 +10,8 @@ class YellowHello extends React.Component {
 
   componentDidMount() {
 
-    this.yellowHello = document.querySelector('.yellow-hello .block');
+    this.yellowHello = document.querySelector('.yellow-hello');
+    this.yellowHelloBlock = document.querySelector('.yellow-hello .block');
     this.trigger1 = document.querySelector('.trigger1');
 
     // initialise the scroll magic
@@ -34,8 +35,13 @@ class YellowHello extends React.Component {
       onReverseComplete: null
     });
 
-    tl.to(this.yellowHello, 0.6, { width: '60%', y: -60 });
-    tl.to(this.yellowHello, 0.6, { opacity: 0, y: 20 });
+    if ((window.innerWidth < 600)) {
+      tl.to(this.yellowHelloBlock, 0.6, { width: '80%', y: -60 });
+    } else {
+      tl.to(this.yellowHelloBlock, 0.6, { width: '60%', y: -60 });
+    }
+    tl.to(this.yellowHelloBlock, 0.6, { opacity: 0, y: 20 });
+    tl.set(this.yellowHello, { display: 'none' });
 
     // declare timeline to controller
     let sc1 = new window.ScrollMagic.Scene({
@@ -43,11 +49,11 @@ class YellowHello extends React.Component {
         triggerHook: 'onLeave',
         duration: '600px'
       })
-      .setTween(tl)
+      .setTween(tl);
       //.addIndicators({name: "tl 1"});
 
     setTimeout(() => {
-      this.props.addToScrollMagicController({'yellowHello': [sc1] });
+      this.props.addToScrollMagicController({ yellowHello: [sc1] });
     }, 0);
 
   }
