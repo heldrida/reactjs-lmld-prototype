e5f1a68b99b21d2d2de38922640b2ada0f10f4ae
Helder <helder@lamoulade.com>
2016-05-11 16:07:07 +0100

    included a font loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
 					'file?hash=sha512&digest=hex&name=[hash].[ext]',
 					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
 				]
+			},
+			{
+				test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+				loader: 'file-loader'
 			}
 		]
 	},
