cf94b85f979755e49febc55fbc91aac026cc40c6
Helder <helder@lamoulade.com>
2016-05-13 11:02:02 +0100

    Removed sourceMaps from the webpack config, to fix relative path issue on Chrome where webfonts triggered an OTS error;

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 	module: {
 		loaders: [
 			{ test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
-            { test: /\.scss$/, loader: 'style!css?sourceMap!sass?sourceMap' },
+            { test: /\.scss$/, loader: 'style!css!sass' },
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,
 				loaders: [
